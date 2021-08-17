bb858ab6a6fd5cc868469dc14aedce66219f23f2
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-08-17 13:50:05 +0530

    Updating the package

--- DataStructures And Algorithms/src/strings/MinSwapsForBracketBalancing.java
+++ DataStructures And Algorithms/src/strings/MinSwapsForBracketBalancing.java
@@ -2,6 +2,14 @@ package strings;
 
 import java.util.Stack;
 
+/**
+ * You are given a string S of 2N characters consisting of N ‘[‘ brackets and N
+ * ‘]’ brackets. A string is considered balanced if it can be represented in the
+ * for S2[S1] where S1 and S2 are balanced strings. We can make an unbalanced
+ * string balanced by swapping adjacent characters. Calculate the minimum number
+ * of swaps necessary to make a string balanced.
+ *
+ */
 public class MinSwapsForBracketBalancing {
 
 	public static void main(String[] args) {
