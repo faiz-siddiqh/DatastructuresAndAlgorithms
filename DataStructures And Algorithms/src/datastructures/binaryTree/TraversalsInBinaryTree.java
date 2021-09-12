45c2bf364c5b68a34bcb068bb5784162c781cb3b
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-09-13 00:03:01 +0530

    Updating the traversals with iterative Solutions

--- DataStructures And Algorithms/src/datastructures/binaryTree/TraversalsInBinaryTree.java
+++ DataStructures And Algorithms/src/datastructures/binaryTree/TraversalsInBinaryTree.java
@@ -1,5 +1,10 @@
 package datastructures.binaryTree;
 
+/**
+ * Perform PreOrder ,PostOrder and InOrder Traversal operations for a given Binary Tree
+ */
+import java.util.Stack;
+
 class Node {
 	int data;
 	Node left;
@@ -11,12 +16,115 @@ class Node {
 }
 
 public class TraversalsInBinaryTree {
+	Node root;
 
 	public TraversalsInBinaryTree() {
 		root = null;
 	}
 
-	Node root;
+	/**
+	 * RECURSION SOLUTIONS FOR TRAVERSALS
+	 * 
+	 * @param Node root
+	 */
+//	private static void postOrderTraversal(Node root) {
+//		if (root == null)
+//			return;
+//
+//		postOrderTraversal(root.left);
+//		postOrderTraversal(root.right);
+//		System.out.print(root.data);
+//	}
+
+//	private static void inOrderTraversal(Node root) {
+//		if (root == null)
+//			return;
+//
+//		inOrderTraversal(root.left);
+//		System.out.print(root.data);
+//		inOrderTraversal(root.right);
+//	}
+//
+//	private static void preOrderTraversal(Node root) {
+//		if (root == null)
+//			return;
+//
+//		System.out.print(root.data);
+//		preOrderTraversal(root.left);
+//
+//		preOrderTraversal(root.right);
+//
+//	}
+
+	/**
+	 * Iterative solution for traversals
+	 * 
+	 * @param root
+	 */
+	private static void inOrderTraversal(Node root) {
+		if (root == null)
+			return;
+		Stack<Node> st = new Stack<>();
+		Node cur = root;
+
+		while (!st.isEmpty() || cur != null) {
+
+			if (cur != null) {
+				st.push(cur);
+				cur = cur.left;
+			} else {
+				cur = st.pop();
+				System.out.print(cur.data + " ");
+				cur = cur.right;
+			}
+
+		}
+
+	}
+
+	private static void preOrderTraversal(Node root) {
+		if (root == null)
+			return;
+
+		Stack<Node> st = new Stack<>();
+		Node cur = root;
+		st.push(cur);
+
+		while (!st.isEmpty()) {
+
+			cur = st.pop();
+			System.out.print(cur.data + " ");
+			if (cur.right != null)
+				st.push(cur.right);
+			if (cur.left != null)
+				st.push(cur.left);
+
+		}
+
+	}
+
+	private static void postOrderTraversal(Node root) {
+		if (root == null)
+			return;
+
+		Stack<Node> stack = new Stack<>();
+		Stack<Node> output = new Stack<>();
+		stack.push(root);
+		Node cur = null;
+		while (!stack.isEmpty()) {
+
+			cur = stack.pop();
+			output.push(cur);
+
+			if (cur.left != null)
+				stack.push(cur.left);
+			if (cur.right != null)
+				stack.push(cur.right);
+
+		}
+		while (!output.isEmpty())
+			System.out.print(output.pop().data + " ");
+	}
 
 	public static void main(String[] args) {
 		TraversalsInBinaryTree tree = new TraversalsInBinaryTree();
@@ -31,35 +139,6 @@ public class TraversalsInBinaryTree {
 		postOrderTraversal(tree.root);
 		System.out.println();
 		inOrderTraversal(tree.root);
-
-	}
-
-	private static void postOrderTraversal(Node root) {
-		if (root == null)
-			return;
-
-		postOrderTraversal(root.left);
-		postOrderTraversal(root.right);
-		System.out.print(root.data);
-	}
-
-	private static void inOrderTraversal(Node root) {
-		if (root == null)
-			return;
-
-		inOrderTraversal(root.left);
-		System.out.print(root.data);
-		inOrderTraversal(root.right);
-	}
-
-	private static void preOrderTraversal(Node root) {
-		if (root == null)
-			return;
-
-		System.out.print(root.data);
-		preOrderTraversal(root.left);
-
-		preOrderTraversal(root.right);
 
 	}
 
