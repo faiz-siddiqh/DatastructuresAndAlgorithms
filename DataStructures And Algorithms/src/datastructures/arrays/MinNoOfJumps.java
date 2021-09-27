6be12056d36ee7c9c4b4867886265adf4afb0b6c
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-09-27 19:57:07 +0530

    pushing changes to datastructures.array

--- DataStructures And Algorithms/src/datastructures/arrays/MinNoOfJumps.java
+++ DataStructures And Algorithms/src/datastructures/arrays/MinNoOfJumps.java
@@ -1,5 +1,12 @@
 package datastructures.arrays;
 
+/**
+ * Given an array of N integers arr[] where each element represents the max
+ * number of steps that can be made forward from that element. Find the minimum
+ * number of jumps to reach the end of the array (starting from the first
+ * element). If an element is 0, then you cannot move through that element.
+ * 
+ */
 public class MinNoOfJumps {
 
 	public static void main(String[] args) {
