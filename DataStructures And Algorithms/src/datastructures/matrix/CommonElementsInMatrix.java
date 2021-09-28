4182c9a08667619bc6b4a0d134365af73ae5a6ab
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-09-28 23:20:33 +0530

    pushing changes to datastructures.matrix package

--- DataStructures And Algorithms/src/datastructures/matrix/CommonElementsInMatrix.java
+++ DataStructures And Algorithms/src/datastructures/matrix/CommonElementsInMatrix.java
@@ -2,17 +2,17 @@ package datastructures.matrix;
 
 import java.util.HashMap;
 
+/**
+ * Given an m x n matrix, find all common elements present in all rows in O(mn)
+ * time and one traversal of matrix.
+ * 
+ * 
+ *
+ */
 public class CommonElementsInMatrix {
 
-	public static void main(String[] args) {
-		int mat[][] = { { 1, 2, 1, 4, 8 }, { 3, 7, 8, 5, 1 }, { 8, 7, 7, 3, 1 }, { 8, 1, 2, 7, 9 }, };
-		int M = 4;
-		int N = 5;
-		printCommonElements(mat, M, N);
-	}
-
 	/**
-	 * Time complexity of this method is O(N)
+	 * Time complexity of this method is O(M*N)
 	 * 
 	 * @param mat
 	 * @param m
@@ -24,9 +24,8 @@ public class CommonElementsInMatrix {
 		for (int i = 0; i < n; i++)
 			map.put(mat[0][i], 1);
 
-		for (int i = 1; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-
+		for (int i = 1; i < m; i++)
+			for (int j = 0; j < n; j++)
 				if (map.get(mat[i][j]) != null && map.get(mat[i][j]) == i) {
 					map.put(mat[i][j], i + 1);
 
@@ -34,9 +33,13 @@ public class CommonElementsInMatrix {
 						System.out.println(mat[i][j] + " ");
 				}
 
-			}
+	}
 
-		}
+	public static void main(String[] args) {
+		int mat[][] = { { 1, 2, 1, 4, 8 }, { 3, 7, 8, 5, 1 }, { 8, 7, 7, 3, 1 }, { 8, 1, 2, 7, 9 }, };
+		int M = 4;
+		int N = 5;
+		printCommonElements(mat, M, N);
 	}
 
 }
