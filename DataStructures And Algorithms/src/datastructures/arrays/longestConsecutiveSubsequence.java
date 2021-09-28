14bc26e01031da2964698734e3ed8428c826e79a
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-09-28 14:30:30 +0530

    adding new method to LongestSubSequence Problem in datastructures.array package

--- DataStructures And Algorithms/src/datastructures/arrays/longestConsecutiveSubsequence.java
+++ DataStructures And Algorithms/src/datastructures/arrays/longestConsecutiveSubsequence.java
@@ -1,6 +1,7 @@
 package datastructures.arrays;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * Given an array of positive integers. Find the length of the longest
@@ -16,12 +17,44 @@ public class longestConsecutiveSubsequence {
 
 		int arr[] = { 10, 20, 21, 22, 23 };
 		int N = arr.length;
-		System.out.println(longestSubsequence(arr, N));
+		System.out.println(longestSubsequenceSol1(arr, N));
 
 	}
-	
-	
-	
+
+	/**
+	 * timeComplexity of this solution is O(logN) and space complexity of O(N) by
+	 * using hashing
+	 * 
+	 * @param arr
+	 * @param n
+	 * @return
+	 */
+	private static int longestSubsequenceSol1(int[] arr, int n) {
+		if (n == 1)
+			return 1;
+		int ans = 1;
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 0; i < n; i++)
+			set.add(arr[i]);
+		for (int i = 0; i < n; i++) {
+
+			if (!set.contains(arr[i] - 1)) { // if set doesnot contain the element it means this is the first element in
+												// the series/sequence
+
+				int j = arr[i];
+
+				while (set.contains(j)) {// check for continous sequence
+					j++;
+				}
+
+				ans = Math.max(ans, j - arr[i]);// update the count
+
+			}
+
+		}
+
+		return ans;
+	}
 
 	/**
 	 * timeComplexity of this solution is O(NlogN)
@@ -43,20 +76,20 @@ public class longestConsecutiveSubsequence {
 		for (int i = 1; i < n; i++) {
 			// Check if the array has duplicates ,if the preceeding number and current
 			// number are same then continue
-			if (arr[i] == arr[j]) 							// --------->1
+			if (arr[i] == arr[j]) // --------->1
 				continue;
 
 			// if the difference is 1,then increment count and point index to current
 			// element
 			if (arr[i] - arr[j] == 1) {
-				curCount++; 								// --------->2
+				curCount++; // --------->2
 				j = i;
 			}
 			// if difference is not 1 ,check if the currentCount > maxCount .then set
 			// curCount to 0 and point index to current element
 			else {
 				if (curCount > maxCount)
-					maxCount = curCount; 					// ---------->3
+					maxCount = curCount; // ---------->3
 				curCount = 0;
 				j = i;
 			}
