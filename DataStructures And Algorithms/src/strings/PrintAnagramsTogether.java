c07c8cbb41278e42a0e4c91a85ecf30986eac8d6
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-09-29 23:55:35 +0530

    pushing PrintAnagramsTogether problem to datastructures.string package

--- DataStructures And Algorithms/src/strings/PrintAnagramsTogether.java
+++ DataStructures And Algorithms/src/strings/PrintAnagramsTogether.java
@@ -6,7 +6,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Given an array of strings, return all groups of strings that are anagrams.
+ * The groups must be created in order of their appearance in the original
+ * array. Look at the sample case for clarification.
+ * 
+ * @author Faiz-Siddiqh
+ *
+ */
 public class PrintAnagramsTogether {
+
+	/**
+	 * Find all the groups of anagrams in the String [] arrr and return a List of
+	 * Collection of anagrams Aprrox TimeComplexity =O(N * Mlog(M)) where M is
+	 * length of each string
+	 * 
+	 * @param arrr
+	 * @return
+	 */
 	public static List<List<String>> Anagrams(String[] arrr) {
 
 		List<List<String>> anagrams = new ArrayList<>();
@@ -42,8 +59,12 @@ public class PrintAnagramsTogether {
 
 		String[] arr = { "act", "god", "cat", "dog", "tac" };
 		List<List<String>> anagrams = Anagrams(arr);
-		
-		for()
+
+		for (List<String> each : anagrams) {
+			for (String eachAnagram : each)
+				System.out.print(eachAnagram + " ");
+			System.out.println();
+		}
 	}
 
 }
