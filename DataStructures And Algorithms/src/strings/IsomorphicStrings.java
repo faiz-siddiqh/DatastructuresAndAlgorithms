beb563d0e70b30028156056966a891af33115ef1
faiz-siddiqh <fasiddiqh72@gmail.com>
2021-10-10 13:02:36 +0530

    pushing changes in IsomorphicStrings problem in strings package

--- DataStructures And Algorithms/src/strings/IsomorphicStrings.java
+++ DataStructures And Algorithms/src/strings/IsomorphicStrings.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.Arrays;
+
 /**
  * Two strings str1 and str2 are called isomorphic if there is a one-to-one
  * mapping possible for every character of str1 to every character of str2. And
@@ -13,8 +15,7 @@ public class IsomorphicStrings {
 	private static int noOfChars = 26;
 
 	/**
-	 * check if the strings are isoMorphic -TimeComplexity of O(N) and space
-	 * Complexity =O(2N)
+	 * check if the strings are isoMorphic -TimeComplexity of O(N)
 	 * 
 	 * @param str1
 	 * @param str2
@@ -40,11 +41,45 @@ public class IsomorphicStrings {
 		return true;
 	}
 
+	/**
+	 * check if the strings are isoMorphic -TimeComplexity of O(N) -another method
+	 * 
+	 * @param str1
+	 * @param str2
+	 * @return
+	 */
+	public static boolean areIsomorphicStrings(String str1, String str2) {
+		if (str1.length() != str2.length() || str1 == null || str2 == null)
+			return false;
+
+		int[] map = new int[noOfChars];
+		Arrays.fill(map, -1);
+		boolean[] isMarked = new boolean[noOfChars];
+		Arrays.fill(isMarked, false);
+
+		for (int i = 0; i < str1.length(); i++) {
+
+			if (map[str1.charAt(i) - 'a'] == -1) {
+
+				if (isMarked[str2.charAt(i) - 'a'] == true)
+					return false;
+
+				isMarked[str2.charAt(i) - 'a'] = true;
+
+				map[str1.charAt(i) - 'a'] = str2.charAt(i);
+			} else if (map[str1.charAt(i) - 'a'] != str2.charAt(i))
+				return false;
+
+		}
+
+		return true;
+	}
+
 	public static void main(String[] args) {
 
 		String str1 = "aab";
-		String str2 = "xxw";
-		System.out.println(areIsomorphic(str1, str2));
+		String str2 = "yyw";
+		System.out.println(areIsomorphicStrings(str1, str2));
 	}
 
 }
