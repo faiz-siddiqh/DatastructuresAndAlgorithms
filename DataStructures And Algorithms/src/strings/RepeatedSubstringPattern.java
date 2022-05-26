035d26ecf47b764bf050b1f40d5319329041c775
faiz-siddiqh <fasiddiqh72@gmail.com>
2022-05-26 23:57:42 +0530

    Pushing repeatedSubstringPattern problem to strings package

---
+++ DataStructures And Algorithms/src/strings/RepeatedSubstringPattern.java
@@ -0,0 +1,18 @@
+package strings;
+
+/**
+ * Given a string s, check if it can be constructed by taking a substring of it
+ * and appending multiple copies of the substring together.
+ * 
+ * @author Faiz-Siddiqh
+ *
+ */
+public class RepeatedSubstringPattern {
+
+	public static void main(String[] args) {
+			
+		
+		
+	}
+
+}
